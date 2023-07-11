4cf1cdf0fcb1ddb48c8101c49c5fff8c4a84cc5b
Bruno Rodrigues <brunorodrigues@gec.inatel.br>
2023-07-11 10:00:08 -0300

    Criando hook para o resumo

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionContext } from "../../contexts/TransactionsContext";
+import { dateFormtter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { TransactionsContainer, TransactionsTable, PriceHightlight } from "./styles";
 
@@ -24,11 +25,12 @@ export function Transactions() {
                   <td width='50%'>{transaction.description}</td>
                   <td>
                     <PriceHightlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHightlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormtter.format(new Date(transaction.createdAt))}</td>
                 </tr>
               )
             })}
